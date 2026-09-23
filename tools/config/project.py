56abe54feb09bff94a88084593878efa06c34e50
TakWolf <takwolf@foxmail.com>
2026-09-23 18:37:08 +0800

    项目级静态配置提取到 project 中

---
+++ tools/config/project.py
@@ -0,0 +1,17 @@
+
+VERSION = '2026.09.01'
+
+FAMILY_NAME_PREFIX = 'Fusion Bold Pixel'
+FILE_NAME_PREFIX = 'fusion-bold-pixel'
+
+DESCRIPTION = 'Open-source Pan-Latin and Pan-CJK pixel font in a bold sans-serif style'
+
+DESIGNER = 'TakWolf'
+DESIGNER_URL = 'https://takwolf.com'
+
+MANUFACTURER = 'TakWolf'
+VENDOR_URL = 'https://pixel-font-studio.github.io/fusion-bold-pixel-font'
+
+COPYRIGHT_INFO = 'Copyright (c) 2026, TakWolf (https://takwolf.com)'
+LICENSE_INFO = 'This Font Software is licensed under the SIL Open Font License, Version 1.1'
+LICENSE_URL = 'https://github.com/pixel-font-studio/fusion-bold-pixel-font/blob/master/LICENSE-OFL'
